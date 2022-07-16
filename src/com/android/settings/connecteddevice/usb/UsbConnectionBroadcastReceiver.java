rcstar6696/platform_packages_apps_Settings

Act as usb device when there is no hal, but we believe we are a device
Change-Id: I036090738525fd8cc63534d52d02ab1852950a7d

src/com/android/settings/connecteddevice/usb/UsbConnectionBroadcastReceiver.java
@@ -77,6 +77,14 @@ public class UsbConnectionBroadcastReceiver extends BroadcastReceiver implements
                 mFunctions = functions;
                 mDataRole = mUsbBackend.getDataRole();
                 mPowerRole = mUsbBackend.getPowerRole();
+                //If we have no USB HAL, mDataRole is invalid
+                //But we can't be connected AND have none data_role, so it's safe.
+		//It would be better to fix UsbManager when no HAL is available, but that's more work
+                if(mDataRole == UsbPort.DATA_ROLE_NONE &&
+                        intent.getExtras().getBoolean(UsbManager.USB_CONNECTED) &&
+                        !intent.getExtras().getBoolean(UsbManager.USB_HOST_CONNECTED))
+                    mDataRole = UsbPort.DATA_ROLE_DEVICE;
+
             }
         } else if (UsbManager.ACTION_USB_PORT_CHANGED.equals(intent.getAction())) {
             UsbPortStatus portStatus = intent.getExtras()

iMOS-OSS/imos_packages_apps_Settings

src/com/android/settings/connecteddevice/usb/UsbConnectionBroadcastReceiver.java
@@ -81,6 +81,13 @@ public class UsbConnectionBroadcastReceiver extends BroadcastReceiver implements
             mFunctions = functions;
             mDataRole = mUsbBackend.getDataRole();
             mPowerRole = mUsbBackend.getPowerRole();
+                //If we have no USB HAL, mDataRole is invalid
+                //But we can't be connected AND have none data_role, so it's safe.
+		//It would be better to fix UsbManager when no HAL is available, but that's more work
+                if(mDataRole == UsbPortStatus.DATA_ROLE_NONE &&
+                        intent.getExtras().getBoolean(UsbManager.USB_CONNECTED) &&
+                        !intent.getExtras().getBoolean(UsbManager.USB_HOST_CONNECTED))
+                    mDataRole = UsbPortStatus.DATA_ROLE_DEVICE;
         } else if (UsbManager.ACTION_USB_PORT_CHANGED.equals(intent.getAction())) {
             UsbPortStatus portStatus = intent.getExtras()
                     .getParcelable(UsbManager.EXTRA_PORT_STATUS);